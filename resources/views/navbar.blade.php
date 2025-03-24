7353bc772c5aa37537b4af24bd613a2508ca321b
Axel Gérard <2442326@cegeprdl.ca>
2025-03-24 14:37:50 -0400

    Creation of views and some routes

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,15 @@
+
+
+<nav class="container">
+    <div class="row row-cols-9">
+        <div class="col col-3"><img src="" alt=""></div>
+        <div class="col"><a href="">Garderies</a></div>
+        <div class="col"><a href="">Dépenses</a></div>
+        <div class="col"><a href="">Commerces</a></div>
+        <div class="col"><a href="">Catégories de dépense</a></div>
+        <div class="col"><a href="">Enfants</a></div>
+        <div class="col"><a href="">Educateurs</a></div>
+        <div class="col"><a href="">Présences</a></div>
+        <div class="col"><a href="">Rapport</a></div>
+    </div>
+</nav>
