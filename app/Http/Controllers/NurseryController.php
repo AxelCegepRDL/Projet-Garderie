adb51b509107141c209d25bf0da888ee3f1d3072
William Lévesque <2343341@cegeprdl.ca>
2025-03-24 14:40:34 -0400

    Start of NurseryController Missing delete() and clear()

---
+++ app/Http/Controllers/NurseryController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Nursery;
+use Illuminate\Http\Request;
+
+class NurseryController extends Controller
+{
+    public function index()
+    {
+        $nurseries = Nursery::all();
+        return view('nursery', compact('nurseries'));
+    }
+
+    public function add(Request $request)
+    {
+        Nursery::create([
+            'name' => $request->name,
+            'address' => $request->address,
+            'city' => $request->city,
+            'state_id' => $request->state_id,
+            'phone' => $request->phone
+        ]);
+    }
+
+    public function formModifyNursery($id)
+    {
+        $nursery = Nursery::findOrFail($id);
+        return view('nurseryModify', compact('nursery'));
+    }
+
+    public function update($id, Request $request)
+    {
+        $nursery = Nursery::findOrFail(1);
+
+        $nursery->name = $request->name;
+        $nursery->address = $request->address;
+        $nursery->city = $request->city;
+        $nursery->state_id = $request->state_id;
+        $nursery->phone = $request->phone;
+
+        $nursery->save();
+    }
+}
